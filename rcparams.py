32db0db5b83e0551f36eacbd3fa264f0aaad6c83
Dimitris Kourtesis <86736107+DimitrisKour@users.noreply.github.com>
2025-10-25 20:51:30 +0300

    Add files via upload

---
+++ rcparams.py
@@ -0,0 +1,55 @@
+# plotting parameters
+rcparams = {
+    "savefig.bbox": "tight",
+    "agg.path.chunksize": 10000,
+    "font.family": "serif",
+    "font.size": 22,
+    "legend.fontsize": 16,
+    "legend.loc": "upper right",
+    "mathtext.fontset": "stix",
+
+    # X axis
+    "xtick.direction": "in",
+    "xtick.major.size": 6,
+    "xtick.major.width": 1,
+    "xtick.minor.size": 3,
+    "xtick.minor.width": 1,
+    "xtick.minor.visible": True,
+    "xtick.top": True,
+
+    # Y axis
+    "ytick.direction": "in",
+    "ytick.major.size": 6,
+    "ytick.major.width": 1,
+    "ytick.minor.size": 3,
+    "ytick.minor.width": 1,
+    "ytick.minor.visible": True,
+    "ytick.right": True,
+
+    # Line widths
+    "axes.linewidth": 0.5,
+    "grid.linewidth": 0.5,
+    "lines.linewidth": 1.0,
+
+    # Grid
+    "axes.grid": True,
+    "axes.axisbelow": True,
+    "grid.linestyle": "--",
+    "grid.color": "k",
+    "grid.alpha": 0.5,
+    "grid.linewidth": 0.5,
+
+    # Legend
+    "legend.frameon": True,
+    "legend.framealpha": 1.0,
+    "legend.fancybox": True,
+    "legend.numpoints": 1,
+
+    # Font sizes
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+    "legend.fontsize": 16,
+    "legend.title_fontsize": 16,
+    "axes.titlesize": 16,
+    "axes.labelsize": 16,
+}
